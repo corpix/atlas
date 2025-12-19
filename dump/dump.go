37482f174c3d286da8fa50190c7c18888ecf6382
Dmitry Moskowski <me@corpix.ru>
2025-12-19 10:40:10 +0000

    dump: add diff facility

--- dump/dump.go
+++ dump/dump.go
@@ -1,12 +1,19 @@
 package dump
 
 import (
+	"fmt"
+
 	"github.com/davecgh/go-spew/spew"
+	"github.com/pmezard/go-difflib/difflib"
 )
 
 var dumper = spew.ConfigState{
-	SortKeys: true,
-	Indent:   "  ",
+	Indent:                  "  ",
+	DisablePointerAddresses: false,
+	DisableCapacities:       false,
+	SortKeys:                true,
+	DisableMethods:          true,
+	MaxDepth:                2,
 }
 
 func Print(xs ...any) {
@@ -15,6 +22,20 @@ func Print(xs ...any) {
 
 func Printf(format string, xs ...any) {
 	_, _ = dumper.Printf(format, xs...)
+}
+
+func Diff(a, b any) {
+	fmt.Println(SDiff(a, b))
+}
+
+func SDiff(a, b any) string {
+	diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+		A:       difflib.SplitLines(dumper.Sdump(a)),
+		B:       difflib.SplitLines(dumper.Sdump(b)),
+		Context: 1,
+	})
+
+	return diff
 }
 
 func Sprintf(format string, xs ...any) string {
--- dump/dump_test.go
+++ dump/dump_test.go
@@ -17,15 +17,25 @@ func Test(t *testing.T) {
 			B string
 		}
 	)
-	assert.Equal(t, "[{foo bar}]", Sprintf("%v", flat{A: "foo", B: "bar"}))
-	assert.Equal(t, "[{A:foo B:bar}]", Sprintf("%+v", flat{A: "foo", B: "bar"}))
-	assert.Equal(t, "[{{foo bar} baz}]", Sprintf("%v", nested{
+	assert.Equal(t, "{foo bar}", Sprintf("%v", flat{A: "foo", B: "bar"}))
+	assert.Equal(t, "{A:foo B:bar}", Sprintf("%+v", flat{A: "foo", B: "bar"}))
+	assert.Equal(t, "{{foo bar} baz}", Sprintf("%v", nested{
 		A: flat{A: "foo", B: "bar"},
 		B: "baz",
 	}))
-	assert.Equal(t, "[{A:{A:foo B:bar} B:baz}]", Sprintf("%+v", nested{
+	assert.Equal(t, "{A:{A:foo B:bar} B:baz}", Sprintf("%+v", nested{
 		A: flat{A: "foo", B: "bar"},
 		B: "baz",
 	}))
 
+	assert.Equal(
+		t,
+		SDiff(flat{A: "a", B: "b"}, flat{A: "a", B: "bb"}),
+		`@@ -2,3 +2,3 @@
+   A: (string) (len=1) "a",
+-  B: (string) (len=1) "b"
++  B: (string) (len=2) "bb"
+ }
+`,
+	)
 }
