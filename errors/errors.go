535d8e4a0192983375ef3f581a4b4d43c60e5b2f
Dmitry Moskowski <me@corpix.ru>
2026-02-09 09:03:23 +0000

    errors: support attaching details to errors

--- errors/errors.go
+++ errors/errors.go
@@ -8,6 +8,8 @@ import (
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/protoadapt"
 )
 
 const (
@@ -24,6 +26,10 @@ var (
 	Errorf = fmt.Errorf
 	New    = errors.New
 )
+
+type RPCDetailer interface {
+	ErrorDetails() []proto.Message
+}
 
 func Log(err error, fmt string, args ...any) {
 	if err != nil {
@@ -49,24 +55,55 @@ func Chain(err error, cause error) error {
 	return Errorf("%w: %w", err, cause)
 }
 
-func RpcCode(err error, code codes.Code, fmt string, args ...any) error {
+func RpcCode(err error, code codes.Code, format string, args ...any) error {
 	if err == nil {
 		return nil
 	}
 
-	log.Error().Err(err).Msgf(fmt, args...)
-	return status.Errorf(code, fmt, args...)
+	log.Error().Err(err).Msgf(format, args...)
+	st := status.New(code, fmt.Sprintf(format, args...))
+	st = RpcDetails(err, st)
+	return st.Err()
 }
 
-func RpcCodeCtx(ctx context.Context, err error, code codes.Code, fmt string, args ...any) error {
+func RpcCodeCtx(ctx context.Context, err error, code codes.Code, format string, args ...any) error {
 	if err == nil {
 		return nil
 	}
 
-	log.Ctx(ctx).Error().Err(err).Msgf(fmt, args...)
-	return status.Errorf(code, fmt, args...)
+	log.Ctx(ctx).Error().Err(err).Msgf(format, args...)
+	st := status.New(code, fmt.Sprintf(format, args...))
+	st = RpcDetails(err, st)
+	return st.Err()
 }
 
-func Rpc(err error, fmt string, args ...any) error {
-	return RpcCode(err, codes.Internal, fmt, args...)
+func Rpc(err error, format string, args ...any) error {
+	return RpcCode(err, codes.Internal, format, args...)
+}
+
+func RpcDetails(err error, st *status.Status) *status.Status {
+	if err == nil || st == nil {
+		return st
+	}
+
+	var detailer RPCDetailer
+	if !errors.As(err, &detailer) {
+		return st
+	}
+
+	details := detailer.ErrorDetails()
+	if len(details) == 0 {
+		return st
+	}
+
+	detailMessages := make([]protoadapt.MessageV1, 0, len(details))
+	for _, detail := range details {
+		detailMessages = append(detailMessages, protoadapt.MessageV1Of(detail))
+	}
+
+	st, err = st.WithDetails(detailMessages...)
+	if err != nil {
+		return st
+	}
+	return st
 }
